c0f88dcee04aab6e2ef90ecc0cd7fda8aff90d84
Waleed <waleed.sohail.1636@gmsil.com>
2022-07-17 13:00:01 +0200

    Correcting Date Format

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
-import axios from "axios";
+import { NavLink } from "react-router-dom";
 function App() {
 
   //set state for question////  
@@ -38,6 +38,33 @@ useEffect(() => {
       )}
 </>
   );
+
+  //Converting Date Formate//
+  function dateFormat(inputDate, format) {
+    //parse the input date
+    const date = new Date(inputDate);
+
+    //extract the parts of the date
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();    
+
+    //replace the month
+    format = format.replace("MM", month.toString().padStart(2,"0"));        
+
+    //replace the year
+    if (format.indexOf("yyyy") > -1) {
+        format = format.replace("yyyy", year.toString());
+    } else if (format.indexOf("yy") > -1) {
+        format = format.replace("yy", year.toString().substr(2,2));
+    }
+
+    //replace the day
+    format = format.replace("dd", day.toString().padStart(2,"0"));
+
+    return format;
+}
+
 
   return (
 
