9524029cda269166e53aa0966aa6940fa6821a9f
Waleed <waleed.sohail.1636@gmsil.com>
2022-07-17 12:31:04 +0200

    create react application

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  
+ 
+  return (
+
+    <div> 
+       
+          <h4 className="heading">PokePol</h4>
+        
+        </div>
+  );
+}
+
+export default App;
