e662d177f52775329eb1304b3c87a005d4d359e8
Waleed <waleed.sohail.1636@gmsil.com>
2022-07-17 12:38:20 +0200

    Calling Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from "react";
 import './App.css';
-
+import axios from "axios";
 function App() {
+
+  //set state for question////  
+const [questions, setQuestions] = useState([]);
+
+//Api Calling////
+useEffect(() => {
+    axios.get('https://polls.apiblueprint.org/questions?page=1', {
+
+    })
+      .then((response) => {
+        console.log(response.data);
+        setQuestions(response.data)
+      }, 
+        (error) => {
+        console.log(error);
+      });
+  }, []);
   
- 
+//Store Data of Poll//
+  const question = (
+    <>
+      {questions.map((question) =>
+          < >
+            <div className="question" key={question.published_at}>
+            <div><b>{question.question + "?"}</b>
+            <br></br>
+            <b className="font">Published on:</b> <span className="font">{dateFormat(question.published_at,'dd-MM-yyyy')} </span>
+            <br></br>
+            <b className="font">Questions:</b> <span className="font">{question.choices.length}</span></div>
+            
+          </div>
+          </>
+
+      )}
+</>
+  );
+
   return (
 
     <div> 
